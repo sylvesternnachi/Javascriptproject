e59e819662c0f1c43c85fa31daf89c6c52b7f4d2
Sylvester <Sylvelity009@yahoo.com>
2022-07-14 07:01:19 +0100

    Added

--- Async/sandbox.js
+++ Async/sandbox.js
@@ -9,15 +9,79 @@ console.log(3);
 console.log(4);
 
 // GETTING DATA FROM API
+const getTodos = (resource, callback) => {
 
-const request = new XMLHttpRequest();
+    const request = new XMLHttpRequest();
 
 request.addEventListener('readystatechange',  ()  => {
     // console.log(request, request.readyState);
-    if(request.readyState === 4){
-        console.log(request.responseText)
+    if(request.readyState === 4 && request.status === 200){
+        const datas = JSON.parse(request.responseText);
+        callback(undefined, datas);
+        // console.log(request.responseText)
+    }else if(request.readyState  === 4) {
+        callback('Could not fetch the data', undefined);
+        // console.log('Could not fetch the data');
     }
 });
+    
+// request.open('GET','https://jsonplaceholder.typicode.com/todos/');
+request.open('GET',resource);
 
-request.open('GET','https://jsonplaceholder.typicode.com/todos/');
 request.send();
+
+};
+
+console.log(5);
+console.log(6);
+
+getTodos('oldtodo.json',(err,data) => {
+
+    if(err){
+        console.log(err)
+    }else{
+        console.log(data);
+    }
+    // console.log(err,data);
+
+    getTodos('newtodo.json',(err,data) => {
+        console.log(data);
+
+        getTodos('todo.json',(err,data) => {
+            console.log(data);
+        });
+
+    });
+
+    
+
+});
+
+console.log(7);
+console.log(8 );
+
+
+
+//Promise Example
+
+const doSomething = () => {
+
+    return new Promise((resolve,reject) => {
+        //fetch something
+        // resolve('some data');
+        reject('some error')
+    });
+    
+};
+
+// doSomething().then((dataxx) => {
+//     console.log(dataxx)
+// },(err) => {
+//     console.log(err);
+// });
+
+doSomething().then((dataxx) => {
+    console.log(dataxx)
+}).catch((err)  => {
+    console.log(err)
+});
